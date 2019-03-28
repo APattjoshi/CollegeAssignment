e80578ab087c49d605e99359a3184d2eb6981634
APattjoshi <47391164+APattjoshi@users.noreply.github.com>
2019-03-28 19:56:37 +0530

    Searching and sorting
    Understanding Searching and sorting

--- SolutionA.java
+++ SolutionA.java
@@ -1,144 +1,50 @@
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.*;
-import java.util.Set;
-import java.util.Map;
-import java.util.Queue;
+package com.company;
+
 import java.util.Scanner;
 
-class Road {
-    public String city1;
-    public String city2;
+public class SolutionA {
 
-    public Road(String city1, String city2) {
-        this.city1 = city1;
-        this.city2 = city2;
-    }
+    //Find the index position method
+    public static int findIndex(int arr[], int start, int end) {
+        if (end >= start) {
+            // Find the mid value of the array
+            int mid = (start + end) / 2;
 
-}
-
-class RoadMap {
-    Map<String, Set<Road>> roadMap = new HashMap<String, Set<Road>>();
-
-    //This function helps to get all the cities in the graph
-    public Set<String> getAllCities() {
-        return this.roadMap.keySet();
-    }
-
-    //This function will read the input
-    public void readLine(String line) {
-        String[] csv = line.split(",");
-        String city1 = csv[0];
-        String city2 = csv[1];
-        addRoad(city1, city2);
-    }
-
-    private void addCity(String city) {
-        this.roadMap.put(city, new HashSet<Road>());
-    }
-
-    //This function will add both outgoing and incoming roads between two cities
-    private void addRoad(String city1, String city2) {
-        Road road1 = new Road(city1, city2);
-        Road road2 = new Road(city2, city1);
-        if (!this.roadMap.containsKey(city1)) {
-            addCity(city1);
-        }
-        if (!this.roadMap.containsKey(city2)) {
-            addCity(city2);
-        }
-        this.roadMap.get(city1).add(road1);
-        this.roadMap.get(city2).add(road2);
-    }
-
-    //This function will return all the outgoing roads from a city
-    public Set<Road> getAllOutgoingRoads(String node) {
-        return this.roadMap.get(node);
-    }
-
-}
-
-public class GraphAssignment {
-    static RoadMap roadMap = new RoadMap();
-
-    public static void readMap(Scanner scanner) {
-        while (true) {
-            String mapLine = scanner.nextLine();
-            if (mapLine.equals("")) {
-                break;
-            }
-            roadMap.readLine(mapLine);
-        }
-        System.out.println("Read map");
-
-    }
-
-
-    public static void findAnyRouteToCity(String source, String destination) {
-        // PART-A: Write the required java code to find the possible routes from source to destination
-        
-        Queue<String> toProcess = new LinkedList<>();
-        Set<String> visitedNodes = new HashSet<>();
-        Map<String, Road> nodeToParentPointer = new HashMap<>();
-
-        // Initial nodes
-        toProcess.add(source);
-        nodeToParentPointer.put(source, null);
-
-        // Traverse the queue
-        while (!toProcess.isEmpty()) {
-            String current = toProcess.remove();
-            visitedNodes.add(current);
-            if (current.equals(destination)) {
-                break;
-            }
-
-            // Relax node
-            for (Road road : roadMap.getAllOutgoingRoads(current)) {
-                if (!visitedNodes.contains(road.city2)){
-                    toProcess.add(road.city2);
-                    nodeToParentPointer.put(road.city2, road);
-                }
+            if (mid == arr[mid]) {
+                return mid;
+            } else if (mid > arr[mid]) {
+                return findIndex(arr, (mid + 1), end);
+            } else {
+                return findIndex(arr, start, (mid - 1));
             }
         }
 
-        // Backtrack final path
-        List<String> path = new LinkedList<>();
-        String current = destination;
-        while (true) {
-            path.add(current);
-            Road parentPointer = nodeToParentPointer.get(current);
-            if (parentPointer == null) {
-                break;
-            }
-            current = parentPointer.city1;
-
-        }
-
-        Collections.reverse(path);
-        StringJoiner sj = new StringJoiner("->");
-        for (String city : path) {
-            sj.add(city);
-        }
-
-            System.out.println(sj);
-        }
-
-
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the Map");
-        readMap(scanner);
-
-        System.out.println("Enter the source ");
-        String source = scanner.nextLine();
-        System.out.println("Enter the destination ");
-        String destination = scanner.nextLine();
-        System.out.println("The route from "+source+" to "+destination+" is");
-
-        findAnyRouteToCity(source, destination);
+        // Return -1 if there is no index matching element value found
+        return -1;
     }
 
+    //Drive method
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        // asking for user input for length of the array
+        int numberOfUserID = input.nextInt();
+        int userId[] = new int[numberOfUserID + 1];
+
+        // insert elements to the empty array by user
+        for (int i = 1; i <= numberOfUserID; i++) {
+            userId[i] = input.nextInt();
+        }
+        input.close();
+
+        //output
+        int N = userId.length; // size of the array
+        int M = findIndex(userId, 1, N - 1); // position of element in the array
+
+        //Display the result
+        if (M == -1) {
+            System.out.println("NOT_FOUND");
+        } else {
+            System.out.println(M);
+        }
+    }
 }
--- SolutionB.java
+++ SolutionB.java
@@ -1,167 +1,67 @@
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.Map;
-import java.util.PriorityQueue;
+package com.company;
+
 import java.util.Scanner;
 
-class Road {
-    public String city1;
-    public String city2;
-    public int safetyScore;
+public class SolutionB {
 
-    public Road(String city1, String city2, int safetyScore) {
-        this.city1 = city1;
-        this.city2 = city2;
-        this.safetyScore = safetyScore;
-    }
-}
-
-class RoadMap {
-    Map<String, Set<Road>> roadMap = new HashMap<String, Set<Road>>();
-
-    //This function helps to get all the cities in the graph	
-    public Set<String> getAllCities() {
-        return this.roadMap.keySet();
-    }
-
-    //This function will read the input
-    public void readLine(String line) {
-        String[] csv = line.split(",");
-        String city1 = csv[0];
-        String city2 = csv[1];
-        int safety = Integer.parseInt(csv[2]);
-        addRoad(city1, city2, safety);
-    }
-
-    private void addCity(String city) {
-        this.roadMap.put(city, new HashSet<Road>());
-    }
-
-    //This function will add both outgoing and incoming roads between two cities
-    private void addRoad(String city1, String city2, int safetyScore) {
-        Road road1 = new Road(city1, city2, safetyScore);
-        Road road2 = new Road(city2, city1, safetyScore);
-        if (!this.roadMap.containsKey(city1)) {
-            addCity(city1);
+    // Search in the array method
+    public static int midValSearch(int arr[], int left, int right, int key) {
+        if (right >= left) {
+            int mid = left + (right - left) / 2; // get the mid value
+            if (arr[mid] == key) 
+                return mid;
+            if (arr[mid] > key)
+                return midValSearch(arr, left, mid - 1, key);
+            return midValSearch(arr, mid + 1, right, key);
         }
-        if (!this.roadMap.containsKey(city2)) {
-            addCity(city2);
-        }
-
-        this.roadMap.get(city1).add(road1);
-        this.roadMap.get(city2).add(road2);
+        return -1; // if value not found return -1
     }
 
-    //This function will return all the outgoing roads from a city
-    public Set<Road> getAllOutgoingRoads(String node) {
-        return this.roadMap.get(node);
-    }
-
-}
-
-public class GraphAssignment {
-
-    static RoadMap roadMap = new RoadMap();
-    static int maxValue = 100000;
-
-    private static void relax(Road road, Map<String, Integer> safety, Map<String, String> safetyPath) {
-        int possible = safety.get(road.city1) + road.safetyScore;
-        if (safety.get(road.city2) > possible) {
-            safety.put(road.city2, possible);
-            safetyPath.put(road.city2, road.city1);
-        }
-    }
-
-    static class SafetyComparator implements Comparator<String> {
-        private final Map<String, Integer> safety;
-
-        public SafetyComparator(Map<String, Integer> safety) {
-            this.safety = safety;
-        }
-
-        public int compare(String c1, String c2) {
-            if (this.safety.get(c1) < this.safety.get(c2)) {
-                return -1;
-            } else if (this.safety.get(c1) > this.safety.get(c2)) {
-                return 1;
-            } else {
-                return 0;
+    // index value finding method
+    public static int findIndex(int arr[], int key) {
+        int start = 0, end = 1; // initialisation of 
+        int val = arr[0];
+        // to avoid out of bound
+        try {
+            while (val <= key) { // updated start and end value            
+                start = end;      //store previous end
+                end = 2 * end;   //double end index to increase the index size
+                val = arr[end]; //update new elements in the array 
             }
-        }
-    }
-
-    public static void readReport(Scanner scanner) {
-        while (true) {
-            String mapLine = scanner.nextLine();
-            if (mapLine.equals("")) {
-                break;
-            }
-            roadMap.readLine(mapLine);
-        }
-
-    }
-
-    public static void findSafestRouteToCity(String source, String destination) {
-        // PART-B: Write the java code to find the safest route between any two cities
-
-        //Note:The report values of safety are between 1-100
-		PriorityQueue<String> toProcess = new PriorityQueue<>();
-        Set<String> visitedNodes = new HashSet<>();
-        Map<String, Road> nodeToParentPointer = new HashMap<>();
-
-        // Initial nodes
-        toProcess.add(source);
-        nodeToParentPointer.put(source, null);
-
-        // Traverse the queue
-        while (!toProcess.isEmpty()) {
-            String current = toProcess.poll();
-            visitedNodes.add(current);
-
-            if (current.equals(destination)) {
-                break;
-            }
-            // Relax node
-            for (Road road : roadMap.getAllOutgoingRoads(current)) {
-                if (!visitedNodes.contains(road.city2)){
-                    toProcess.add(road.city2);
-                    nodeToParentPointer.put(road.city2, road);
+            return midValSearch(arr, start, end, key); // execute if the double end is not out of bound
+        } catch (ArrayIndexOutOfBoundsException e) {
+            //if end is out of bound execute following
+            while (val <= key) {
+                val = arr[start];
+                if (val == key) {
+                    break;
                 }
+                start++;
             }
+            return start;
         }
-        //backtracking
-        List<String> path = new ArrayList<>();
-        String current = destination;
-        while (true) {
-            path.add(current);
-            Road parentPointer = nodeToParentPointer.get(current);
-            if (parentPointer == null) {
-                break;
-            }
-            current = parentPointer.city1;
-        }
-        Collections.reverse(path);
-        StringJoiner sj = new StringJoiner("->");
-        for (String city : path) {
-            sj.add(city);
-        }
-        System.out.println(sj);
-
     }
 
+    // Driver method
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the graph");
-        readReport(scanner);
+        Scanner input = new Scanner(System.in);
+        // Asking for user input
+        int numberOfUserID = input.nextInt();
+        int key = input.nextInt();
+        int userId[] = new int[numberOfUserID + 1]; // indexing from 1
 
-        System.out.println("Enter the source ");
-        String source = scanner.nextLine();
-        System.out.println("Enter the destination ");
-        String destination = scanner.nextLine();
-        System.out.println("The route from "+source+" to "+destination+" is");
+        // insert element to the empty array
+        for (int i = 1; i <= numberOfUserID; i++) {
+            userId[i] = input.nextInt();
+        }
+        input.close();
 
-        findSafestRouteToCity(source, destination);
+        int M = findIndex(userId, key); // in the user searched value finding the user id position from the array.
+
+        if (M == -1) {
+            System.out.println("NOT_FOUND");
+        } else {
+            System.out.println(M);
+        }
     }
 }
